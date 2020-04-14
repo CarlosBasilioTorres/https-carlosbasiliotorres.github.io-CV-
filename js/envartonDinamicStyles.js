1834d9b88a0e14bfccbcf6d001a61a1e31edb332
CarlosBasilioTorres <carlos.basilo@outlook.es>
2020-04-13 22:40:24 -0500

    agregando carrusel a contactos

--- js/envartonDinamicStyles.js
+++ js/envartonDinamicStyles.js
@@ -1,3 +1,7 @@
+let items = 0;
+let contador = 1;
+let desplazamientos = [];
+
 function showMobileMenu(){
     let menu = document.getElementsByClassName("container-sidenav");
 
@@ -20,12 +24,68 @@ function hideMobileMenu(){
     console.log(menu);  
 }
 
+function previus(){
+    for(let i = 0; i < items; i++){
+        desplazamientos[i] -= 100;
+        if(desplazamientos[i] == -300){
+            desplazamientos[i] = 100;
+        }
+    }    
 
+    
+    for(let i = 0; i < items;i++){
+        document.getElementById("item-" + (i+1)).style.left = desplazamientos[i] + "%";
+    }
 
-function envartonDinamicStyles(datos = {}){
-    if(datos.carrousel){
-        alert("encontre un carrusel");
-    }else{
-        alert("no encontre nada");
+    console.table(desplazamientos)
+}
+
+function next(){    
+    contador++;
+
+    if(contador > items){
+        contador = 1;
+    }
+
+    for(let i = 0; i < items;i++){
+        desplazamientos[i] += 100;    
+        if(desplazamientos[i] == 200){
+            desplazamientos[i] = -200;
+        }
+    }
+    
+    for(let i = 0; i < items;i++){
+        document.getElementById("item-" + (i+1)).style.left = desplazamientos[i] + "%";
+        let aux = i+1;
+        if(aux == contador){
+            document.getElementById("item-"+(i+1)).style.opacity = 1;
+        }else{
+            document.getElementById("item-"+(i+1)).style.opacity = 0;
+        }
+    }
+    
+    console.table(desplazamientos)
+}
+
+function carrousel(datos = {}){
+    items = datos.items;
+
+    for(let i = 0; i < datos.items;i++){
+        desplazamientos.push(-100 *i);
+        if(desplazamientos[i] == -300){
+            desplazamientos[i]=100;
+        }
+    }
+    
+
+    for(let i = 0; i < datos.items;i++){        
+        document.getElementById("item-"+(i+1)).style.left = desplazamientos[i] + "%";
+        let aux = i+1;
+        if(aux == contador){
+            document.getElementById("item-"+(i+1)).style.opacity = 1;
+        }else{
+            document.getElementById("item-"+(i+1)).style.opacity = 0;
+        }
     }
 }
+
